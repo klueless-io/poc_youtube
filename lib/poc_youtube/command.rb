3fb5ff7f9d38d88ee73e20bd06eb885cfdbe579e
David <david.cruwys@bugcrowd.com>
2020-01-10 07:49:25 +1100

    add command and google credentials

--- lib/poc_youtube/command.rb
+++ lib/poc_youtube/command.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
+require_relative 'google_credentials'
+
 require 'forwardable'
+
+require 'google/apis'
+require 'google/apis/youtube_v3'
+require 'googleauth'
+require 'googleauth/stores/file_token_store'
+
+require 'tty-table'
 
 module PocYoutube
   # Base command class
@@ -118,5 +127,59 @@ module PocYoutube
       require 'tty-which'
       TTY::Which.exist?(*args)
     end
+
+    # Helpers for Google Auth and API calls
+        def google_credentials
+      return @google_credentials if defined? @google_credentials
+
+      @google_credentials = begin
+        auth = PocYoutube::GoogleCredentials.for_youtube
+
+        refresh_token_file = '/Users/davidcruwys/dev_credentials/poc-youtube-refresh-token.json'
+
+        refresh_token = auth.load_refresh_token_file(refresh_token_file)
+        auth.load_user_refresh_credentials(refresh_token)
+      end
+    end
+
+    def youtube
+      return @youtube if defined? @youtube
+
+      @youtube = begin
+        youtube = Google::Apis::YoutubeV3::YouTubeService.new
+        youtube.authorization = google_credentials
+        youtube
+      end
+    end
+
+    # Helpers for printing out some data
+    def pretty(data)
+      heading(JSON.pretty_generate(data))
+    end
+
+    def heading(heading)
+      puts '-' * 70
+      puts heading
+      puts '-' * 70
+    end
+    
+    def print_all(data)
+      keys = data.first.keys
+      data.each do |row|
+        keys.each do |key|
+          puts "#{key.to_s.rjust(20)}: #{row[key].to_s.delete("\n")[1..100]}"
+        end
+        puts '-'*120
+      end
+    end
+
+    def pretty_table(heading, column_headings, column_values)
+      heading heading
+
+      table = TTY::Table.new column_headings, column_values
+      puts table.render(:unicode, multiline: true, resize: true)
+    end
+
+
   end
 end
